6cbbdf901e62c7d9859200dc10ef72910b42f1e3
AndroidHappier <51598016+CharanRayudu@users.noreply.github.com>
2021-07-24 01:09:20 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,27 @@ const swup = new Swup();
 update();
 swup.on('contentReplaced', update);
 
+blink();
+var temp = 0;
+
+function toggleUnderScore() {
+    console.log(temp);
+    if (temp == 0) {
+        document.getElementById('underscore').classList.add('hidden');
+        temp = 1;
+    } else {
+        document.getElementById('underscore').classList.remove('hidden');
+        temp = 0;
+    }
+}
+
+function blink() {
+    setInterval(function() {
+        toggleUnderScore();
+    }, 1000);
+}
+
+
 
 function update() {
     if (window.location.href == "https:charanrayudu.github.io/index.html") {
@@ -130,25 +151,6 @@ function update() {
     /*--/ End Scrolling nav /--*/
 
     /*--/ Navbar Menu Reduce /--*/
-    $(window).trigger('scroll');
-    $(window).on('scroll', function() {
-        var pixels = 50;
-        var top = 1200;
-        if ($(window).scrollTop() > pixels) {
-            $('.navbar-expand-md').addClass('navbar-reduce');
-            $('.navbar-expand-md').removeClass('navbar-trans');
-        } else {
-            if (!$('#navbarDefault').hasClass('show')) {
-                $('.navbar-expand-md').removeClass('navbar-reduce');
-            }
-            $('.navbar-expand-md').addClass('navbar-trans');
-        }
-        if ($(window).scrollTop() > top) {
-            $('.scrolltop-mf').fadeIn(1000, "easeInOutExpo");
-        } else {
-            $('.scrolltop-mf').fadeOut(1000, "easeInOutExpo");
-        }
-    });
 
     /*--/ Star Typed /--*/
     if ($('.text-slider').length == 1) {
